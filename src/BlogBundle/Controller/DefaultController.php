fd365fe772a465b5799e7569b86e962ce2cefbb3
thecoon <antobarth@gmail.com>
2016-12-16 17:09:01 +0100

    clean up

--- src/BlogBundle/Controller/DefaultController.php
+++ src/BlogBundle/Controller/DefaultController.php
@@ -3,15 +3,32 @@
 namespace BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {   $user = $this->getUser();
 
-        $posts = $this->getDoctrine()->getRepository('BlogBundle:Post')->findBy(array(),array('postedDate' => 'DESC'));
+        // $posts = $this->getDoctrine()->getRepository('BlogBundle:Post')->findBy(array(),array('postedDate' => 'DESC'));
+        $em = $this->getDoctrine()->getManager();
+        $dql = "SELECT a FROM BlogBundle:Post a ORDER BY a.postedDate DESC";
+        $query = $em->createQuery($dql);
 
-        return $this->render('BlogBundle:Default:index.html.twig',array('user' => $user, 'posts' => $posts ));
+        $paginator = $this->get('knp_paginator');
+
+        $pagination = $paginator->paginate(
+          $query,
+          $request->query->get('page',1),
+          3
+        );
+
+
+        return $this->render('BlogBundle:Default:index.html.twig',array('user' => $user,
+        // 'posts' => $posts,
+        'pagination' => $pagination
+       ));
     }
 
     public function handleExceptionAction(){
@@ -19,7 +36,7 @@ class DefaultController extends Controller
       return $this->render('BlogBundle:Default:404.html.twig',array('user'=>$user));
     }
 
-    
+
     // public function confirmedAction() {
     //   return $this->redirectToRoute('blog_homepage');
     // }
--- src/BlogBundle/Controller/PostController.php
+++ src/BlogBundle/Controller/PostController.php
@@ -9,66 +9,90 @@ use Symfony\Component\HttpFoundation\Request;
 
 class PostController extends Controller
 {
-    public function createPostAction(Request $request)
-    {
-        $user = $this->getUser();
-        $post = new Post();
-        $post->initScore();
-        $post->setPostedDate(new \DateTime());
-        $post->setUserPost($user);
+  public function createPostAction(Request $request,$edit)
+  {
+    $user = $this->getUser();
+    $post = new Post();
+    $post->initScore();
+    $post->setPostedDate(new \DateTime());
+    $post->setUserPost($user);
 
-        $form = $this->createForm(PostType::class,$post);
+    $form = $this->createForm(PostType::class,$post);
 
-        $form->handleRequest($request);
+    $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()){
-          $em = $this->getDoctrine()->getManager();
-          $em->persist($post);
-          $em->flush();
+    if($form->isSubmitted() && $form->isValid()){
+      $em = $this->getDoctrine()->getManager();
+      $em->persist($post);
+      $em->flush();
 
-          return $this->redirect($this->generateUrl(
-            'blog_homepage',array()
-          ));
-        }
-
-
-        return $this->render('BlogBundle:Post:create_post.html.twig', array(
-            'form' => $form->createView(),
-        ));
+      return $this->redirect($this->generateUrl(
+        'blog_homepage',array()
+      ));
     }
 
-    public function mybookPostAction(){
-      $user = $this->getUser();
-      $posts = $user->getUserBook();
-      return $this->render('BlogBundle:Post:my_book.html.twig', array('user' => $user, 'posts' => $posts));
-    }
 
-    public function showPostAction($id)
-    {   $user = $this->getUser();
-        $em = $this->getDoctrine()->getManager();
-        $post = $em->getRepository('BlogBundle:Post')->find($id);
+    return $this->render('BlogBundle:Post:create_post.html.twig', array(
+      'form' => $form->createView(),
+    ));
+  }
 
-        return $this->render('BlogBundle:Post:show_post.html.twig', array( 'post' => $post, 'user' => $user
-        ));
-    }
+  public function mybookPostAction(){
+    $user = $this->getUser();
+    $posts = $user->getUserBook();
+    return $this->render('BlogBundle:Post:my_book.html.twig', array('user' => $user, 'posts' => $posts));
+  }
 
-    public function deletePostAction($id)
-    {
-        // $user = $this.getUser();
-        $em = $this->getDoctrine()->getManager();
-        $post = $em->getRepository('BlogBundle:Post')->find($id);
-        $em->remove($post);
-        $em->flush();
+  public function showPostAction($id)
+  {   $user = $this->getUser();
+    $em = $this->getDoctrine()->getManager();
+    $post = $em->getRepository('BlogBundle:Post')->find($id);
+    $post->upvote();
+    $em->persist($post);
+    $em->flush();
 
-        return $this->redirectToRoute('mybook_post');
+    return $this->render('BlogBundle:Post:show_post.html.twig', array( 'post' => $post, 'user' => $user
+  ));
+}
 
-    }
+public function deletePostAction($id)
+{
+  // TODO sécuriser le delete des posts
+  // $user = $this.getUser();
+  $em = $this->getDoctrine()->getManager();
+  $post = $em->getRepository('BlogBundle:Post')->find($id);
+  $em->remove($post);
+  $em->flush();
 
-    public function listPostAction()
-    {
-        return $this->render('BlogBundle:Post:list_post.html.twig', array(
-            // ...
-        ));
-    }
+  return $this->redirectToRoute('mybook_post');
+
+}
+
+public function updatePostAction(Request $request,$id)
+{
+  $post = $this->getDoctrine()->getRepository('BlogBundle:Post')->find($id);
+  $user = $this->getUser();
+  if(!$user->getUserBook()->contains($post)){
+    return $this->redirect($this->generateUrl(
+      'blog_homepage',array()
+    ));
+  }
+  $form = $this->createForm(PostType::class,$post);
+  $form->handleRequest($request);
+  if($form->isSubmitted() && $form->isValid()){
+    $em = $this->getDoctrine()->getManager();
+    $em->persist($post);
+    $em->flush();
+
+    return $this->redirect($this->generateUrl(
+      'blog_homepage',array()
+    ));
+  }
+  
+
+  return $this->render('BlogBundle:Post:update_post.html.twig', array(
+    'form' => $form->createView()
+  ));
+}
 
 }
